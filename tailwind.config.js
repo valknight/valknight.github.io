92e34f440b8749380cad30b81ab780c00cc34ec3
Val Knight <val@valknight.xyz>
2023-07-02 13:22:10 +0100

    massive overhaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ["./docs/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'cards-dualpets': "url('img/mr-business-blur-crop.webp')",
+                'cards-snipandshear': "url('img/snip-and-shear.webp')",
+                'cards-debugger': "url('img/debugger.webp')"
+            },
+            colors: {
+                'theme-dark': '#32272F',
+                'theme-light': '#D8D6D2'
+              },        
+        },
     },
     plugins: [],
 }
